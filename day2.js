d96518d421ce2fa5a93e556cfd86a1c53ff66a04
adam.tyler <lazy9669@gmail.com>
2016-06-20 12:38:27 -0500

    Day 2 part 2

--- day2.js
+++ day2.js
@@ -19,14 +19,41 @@ plus 1 square foot of slack, for a total of 43 square feet.
 All numbers in the elves' list are in feet. How many total square feet of wrapping paper
 should they order?
 
+
+--- Part Two ---
+
+The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry
+about the length they need to order, which they would again like to be exact.
+
+The ribbon required to wrap a present is the shortest distance around its sides, or the smallest
+perimeter of any one face. Each present also requires a bow made out of ribbon as well; the feet of
+ribbon required for the perfect bow is equal to the cubic feet of volume of the present. Don't
+ask how they tie the bow, though; they'll never tell.
+
+For example:
+
+A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap
+the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the
+present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+How many total feet of ribbon should they order?
+
+
 *******/
 
 var fs = require('fs');
 var readline = require('readline');
 var sqftPaper = 0;
+var lengthRibbon = 0;
 
 function getSurfaceArea(l, w, h) {
   return 2*l*w + 2*w*h + 2*h*l;
+}
+
+function getRibbon(l, w, h) {
+  var length = Math.min(2*l + 2*w, 2*l + 2*h, 2*w + 2*h);
+  length += l*w*h;
+  return length;
 }
 
 var lineReader = readline.createInterface({
@@ -41,8 +68,10 @@ lineReader.on('line', function(line) {
   var smallest = Math.min(length*width, length*height, width*height);
 
   sqftPaper += getSurfaceArea(length, width, height) + smallest;
+  lengthRibbon += getRibbon(length, width, height);
 });
 
 lineReader.on('close', function() {
   console.log('The elves need ' + sqftPaper + ' sqft of paper');
+  console.log('The elves need ' + lengthRibbon + ' feet of ribbon');
 });
