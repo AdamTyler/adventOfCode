4018f3fca26704b5f548ee3aed2af59060487086
adam.tyler <lazy9669@gmail.com>
2016-06-23 10:49:24 -0500

    Day 5 part 2

--- day5.js
+++ day5.js
@@ -22,16 +22,40 @@ haegwjzuvuyypxyu is naughty because it contains the string xy.
 dvszwmarrgswjxmb is naughty because it contains only one vowel.
 How many strings are nice?
 
+--- Part Two ---
+
+Realizing the error of his ways, Santa has switched to a better model of determining whether a
+string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+
+Now, a nice string is one with all of the following properties:
+
+It contains a pair of any two letters that appears at least twice in the string without
+overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+It contains at least one letter which repeats with exactly one letter between them, like xyx,
+abcdefeghi (efe), or even aaa.
+For example:
+
+qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that
+repeats with exactly one letter between them (zxz).
+xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between,
+even though the letters used by each rule overlap.
+uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between
+them.
+ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair
+that appears twice.
+How many strings are nice under these new rules?
 
 ***************/
 
 var fs = require('fs');
 var readline = require('readline');
 var niceStrings = 0;
+var niceStrings2 = 0;
 var lineReader = readline.createInterface({
   input: fs.createReadStream('./inputs/day5.txt'),
 });
 
+// ----- Part 1 Functions -----
 var checkVowels = function(line) {
 
   var numVowels = 0;
@@ -71,13 +95,38 @@ var badStrings = function(line) {
   return false;
 };
 
-lineReader.on('line', function(line) {
+// ----- Part 2 Functions -----
+var twoTwice = function(line) {
+  for(var i = 0, max = line.length; i < max; i++) {
+    var checkFor = line[i] + line[i+1];
+    for(var j = i+2; j < max; j++) {
+      if((line[j] + line[j+1]) === checkFor) {
+        return true;
+      }
+    }
+  }
+};
 
+var oneBetween = function(line) {
+  for(var i = 0, max = line.length; i < max; i++) {
+    if(line[i] === line[i+2]) {
+      return true;
+    }
+  }
+};
+
+lineReader.on('line', function(line) {
+  // Part 1
   if(checkVowels(line) && checkTwice(line) && !badStrings(line)) {
     niceStrings += 1;
+  }
+  // Part 2
+  if(twoTwice(line) && oneBetween(line)) {
+    niceStrings2 += 1;
   }
 });
 
 lineReader.on('close', function() {
-  console.log('There are ' + niceStrings + ' nice strings');
+  console.log('There are ' + niceStrings + ' nice strings in Part1');
+  console.log('There are ' + niceStrings2 + ' nice strings in Part2');
 })
