84edeea7008ffdef6e724d1b21e172aaa750b23c
adam.tyler <lazy9669@gmail.com>
2016-06-27 21:46:40 -0500

    Day 8 part 2

--- day8.js
+++ day8.js
@@ -32,6 +32,23 @@ For example, given the four strings above, the total number of characters of str
 (2 + 5 + 10 + 6 = 23) minus the total number of characters in memory for string values
 (0 + 3 + 7 + 1 = 11) is 23 - 11 = 12.
 
+--- Part Two ---
+
+Now, let's go the other way. In addition to finding the number of characters of code, you should
+now encode each code representation as a new string and find the number of characters of the new
+encoded representation, including the surrounding double quotes.
+
+For example:
+
+"" encodes to "\"\"", an increase from 2 characters to 6.
+"abc" encodes to "\"abc\"", an increase from 5 characters to 9.
+"aaa\"aaa" encodes to "\"aaa\\\"aaa\"", an increase from 10 characters to 16.
+"\x27" encodes to "\"\\x27\"", an increase from 6 characters to 11.
+Your task is to find the total number of characters to represent the newly encoded strings minus
+the number of characters of code in each original string literal. For example, for the strings
+above, the total encoded length (6 + 9 + 16 + 11 = 42) minus the characters in the original code
+representation (23, just like in the first part of this puzzle) is 42 - 23 = 19.
+
 ********/
 
 var fs = require('fs');
@@ -43,6 +60,7 @@ var lineReader = readline.createInterface({
 
 var memoryChars = 0;
 var stringChars = 0;
+var partTwo = 0;
 
 lineReader.on('line', function(line) {
   line = line.trim();
@@ -60,8 +78,11 @@ lineReader.on('line', function(line) {
       }
     }
   }
+  // Part 2 simplified
+  partTwo += JSON.stringify(line).length - line.length;
 });
 
 lineReader.on('close', function() {
   console.log('Difference is:', memoryChars - stringChars);
-})
+  console.log('Part 2 Difference is:', partTwo);
+});
