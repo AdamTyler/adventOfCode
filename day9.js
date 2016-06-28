93fa015c631f69eb4e529a5d2706214f275e0e31
adam.tyler <lazy9669@gmail.com>
2016-06-28 14:39:25 -0500

    Day 9 part 1

--- day9.js
+++ day9.js
@@ -27,3 +27,63 @@ example.
 What is the distance of the shortest route?
 
 ****************/
+
+var fs = require('fs');
+var readline = require('readline');
+
+var lineReader = readline.createInterface({
+  input: fs.createReadStream('./inputs/day9.txt')
+});
+
+var cities = [];
+var distances = {};
+
+lineReader.on('line', function(line) {
+  var match = line.match(/^(\w+) to (\w+) = (\d+)/);
+  if (match) {
+    if(cities.indexOf(match[1]) === -1) { cities.push(match[1]); }
+    if(cities.indexOf(match[2]) === -1) { cities.push(match[2]); }
+    distances[match[1]] = distances[match[1]] || {};
+    distances[match[1]][match[2]] = parseInt(match[3]);
+    distances[match[2]] = distances[match[2]] || {};
+    distances[match[2]][match[1]] = parseInt(match[3]);
+  }
+});
+
+function perms(input) {
+  var permArr = [];
+  var usedChars = [];
+  return (function main() {
+    for (var i = 0; i < input.length; i++) {
+      var ch = input.splice(i, 1)[0];
+      usedChars.push(ch);
+      if (input.length === 0) {
+        permArr.push(usedChars.slice());
+      }
+      main();
+      input.splice(i, 0, ch);
+      usedChars.pop();
+    }
+    return permArr;
+  })();
+}
+
+lineReader.on('close', function() {
+  var max = -1;
+  var min = Number.MAX_VALUE;
+
+  var cityPerms = perms(cities);
+  for (var i = 0; i < cityPerms.length; i++) {
+    var thisTry = cityPerms[i];
+    var total = 0;
+
+    for (var j = 0; j < thisTry.length - 1; j++) {
+      total += distances[thisTry[j]][thisTry[j+1]];
+    }
+
+    if (total < min) {
+      min = total;
+    }
+  }
+  console.log('Shortest distance is:', min);
+});
