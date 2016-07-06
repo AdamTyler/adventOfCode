7e9184a9c05de282fd4507ca5c23ea778780155e
adam.tyler <lazy9669@gmail.com>
2016-07-05 22:01:59 -0500

    Day 13 part 2

--- day13.js
+++ day13.js
@@ -43,13 +43,26 @@ is the most optimal, with a total change in happiness of 330.
 
 What is the total change in happiness for the optimal seating arrangement of the actual guest list?
 
+
+--- Part Two ---
+
+In all the commotion, you realize that you forgot to seat yourself. At this point, you're pretty
+apathetic toward the whole thing, and your happiness wouldn't really go up or down regardless of
+who you sit next to. You assume everyone else would be just as ambivalent about sitting next to
+you, too.
+
+So, add yourself to the list, and give all happiness relationships that involve you a score of 0.
+
+What is the total change in happiness for the optimal seating arrangement that actually includes
+yourself?
+
 ************/
 
 var fs = require('fs');
 var regex = /(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)/
 var readline = require('readline');
 var input = {};
-var people = [];
+var people = ['Me'];
 var lineReader = readline.createInterface({
   input: fs.createReadStream('./inputs/day13.txt')
 });
@@ -63,7 +76,10 @@ lineReader.on('line', function(line) {
   if (people.indexOf(to) === -1) { people.push(to); }
   input[from] = input[from] || {};
   input[to] = input[to] || {};
+  input['Me'] = input['Me'] || {};
   input[from][to] = type * parseInt(match[3]);
+  input[from]['Me'] = 0;
+  input['Me'][to] = 0;
 });
 
 function perms(input) {
